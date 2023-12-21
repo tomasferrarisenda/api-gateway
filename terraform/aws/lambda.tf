7d9125ff0a84e321eb9b7a0c59ac07223ece7f11
Tomas Ferrari <tferrari.92@gmail.com>
2023-12-21 19:38:44 +0100

    cleanup

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -25,23 +25,12 @@ resource "aws_lambda_function" "lambda_sum" {
   source_code_hash = filebase64sha256("./templates/lambda_sum.zip")
 }
 
-resource "aws_lambda_function" "hello_lambda" {
-  filename         = "./templates/hello_lambda_function.zip"
-  function_name    = "hello_lambda"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "hello_lambda_function.lambda_handler"
-  runtime          = "python3.8"
+# resource "aws_lambda_function" "hello_lambda" {
+#   filename         = "./templates/hello_lambda_function.zip"
+#   function_name    = "hello_lambda"
+#   role             = aws_iam_role.lambda_role.arn
+#   handler          = "hello_lambda_function.lambda_handler"
+#   runtime          = "python3.8"
 
-  source_code_hash = filebase64sha256("./templates/hello_lambda_function.zip")
-}
-
-
-# resource "aws_lambda_function" "lambda_hello_world" {
-#   filename      = "./templates/lambda_hello_world.zip"
-#   function_name = "lambda_hello_world"
-#   role          = aws_iam_role.lambda_role.arn
-# #   handler       = "lambda_function.lambda_handler"
-#   runtime       = "python3.8"
-
-#   source_code_hash = filebase64sha256("./templates/lambda_hello_world.zip")
+#   source_code_hash = filebase64sha256("./templates/hello_lambda_function.zip")
 # }
