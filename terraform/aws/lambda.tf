820239d2db8f1a41a6560a932616d65b5514ef28
Tomas Ferrari <tferrari.92@gmail.com>
2023-12-21 19:27:43 +0100

    cleanup

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -17,12 +17,21 @@ resource "aws_lambda_function" "lambda_sum" {
   filename      = "./templates/lambda_sum.zip"
   function_name = "lambda_sum"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_function.lambda_handler"
+  handler       = "lambda_sum.lambda_handler"
   runtime       = "python3.8"
 
   source_code_hash = filebase64sha256("./templates/lambda_sum.zip")
 }
 
+resource "aws_lambda_function" "hello_lambda" {
+  filename         = "./templates/hello_lambda_function.zip"
+  function_name    = "hello_lambda"
+  role             = aws_iam_role.hello_lambda_role.arn
+  handler          = "hello_lambda_function.lambda_handler"
+  runtime          = "python3.8"
+
+  source_code_hash = filebase64sha256("./templates/hello_lambda_function.zip")
+}
 # resource "aws_lambda_function" "lambda_hello_world" {
 #   filename      = "./templates/lambda_hello_world.zip"
 #   function_name = "lambda_hello_world"
