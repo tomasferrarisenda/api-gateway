fddca6266d5ebe7f460de00d3aadc95986df4bec
Tomas Ferrari <tferrari.92@gmail.com>
2023-12-22 00:22:52 +0100

    cleanup

--- terraform/aws/security-groups.tf
+++ terraform/aws/security-groups.tf
@@ -1,21 +1,21 @@
-# resource "aws_security_group" "default" {
-#   name_prefix = var.project
-#   vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "default" {
+  name_prefix = var.project
+  vpc_id      = aws_vpc.main.id
 
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 # resource "aws_security_group" "databases" {
 #   name_prefix = var.project
