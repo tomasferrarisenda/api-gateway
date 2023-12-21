cc643e01b878d5c4e2c4fef115712c35123bfa0b
Tomas Ferrari <tferrari.92@gmail.com>
2023-12-21 19:05:21 +0100

    cleanup

--- terraform/aws/provider.tf
+++ terraform/aws/provider.tf
@@ -34,15 +34,15 @@ provider "aws" {
 
 # ----------------- Helm -----------------
 
-data "aws_eks_cluster_auth" "default" {
-  name = aws_eks_cluster.cluster.id
-}
+# data "aws_eks_cluster_auth" "default" {
+#   name = aws_eks_cluster.cluster.id
+# }
 
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+# provider "helm" {
+#   kubernetes {
+#     host                   = aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
 
-    token = data.aws_eks_cluster_auth.default.token
-  }
-}
+#     token = data.aws_eks_cluster_auth.default.token
+#   }
+# }
