110a4f18e4cbe1e8cd257f36c97627a4e1b91c15
Tomas Ferrari <tferrari.92@gmail.com>
2023-12-23 03:47:39 +0100

    probando

--- terraform/aws/elastic-beanstalk.tf
+++ terraform/aws/elastic-beanstalk.tf
@@ -1,83 +1,83 @@
-resource "aws_iam_role" "eb_role" {
-  name = "my-eb-role"
+# resource "aws_iam_role" "eb_role" {
+#   name = "my-eb-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "eb_role_policy" {
-  role       = aws_iam_role.eb_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkReadOnly"  # Updated policy ARN
-}
-
-
-resource "aws_iam_instance_profile" "eb_instance_profile" {
-  name = "my-eb-instance-profile"
-  role = aws_iam_role.eb_role.name
-}
+# resource "aws_iam_role_policy_attachment" "eb_role_policy" {
+#   role       = aws_iam_role.eb_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkReadOnly"  # Updated policy ARN
+# }
 
 
+# resource "aws_iam_instance_profile" "eb_instance_profile" {
+#   name = "my-eb-instance-profile"
+#   role = aws_iam_role.eb_role.name
+# }
 
-resource "aws_elastic_beanstalk_application" "myapp" {
-  name        = "my-express-app"
-  description = "My Express Application"
-}
 
-resource "aws_elastic_beanstalk_environment" "myenv" {
-  name                = "my-express-app-env"
-  application         = aws_elastic_beanstalk_application.myapp.name
-  solution_stack_name = "64bit Amazon Linux 2023 v6.0.4 running Node.js 18"  # Choose the relevant solution stack
+
+# resource "aws_elastic_beanstalk_application" "myapp" {
+#   name        = "my-express-app"
+#   description = "My Express Application"
+# }
+
+# resource "aws_elastic_beanstalk_environment" "myenv" {
+#   name                = "my-express-app-env"
+#   application         = aws_elastic_beanstalk_application.myapp.name
+#   solution_stack_name = "64bit Amazon Linux 2023 v6.0.4 running Node.js 18"  # Choose the relevant solution stack
+
+# #   setting {
+# #     namespace = "aws:elasticbeanstalk:container:nodejs"
+# #     name      = "NodeCommand"
+# #     value     = "npm start"
+# #   }
 
 #   setting {
-#     namespace = "aws:elasticbeanstalk:container:nodejs"
-#     name      = "NodeCommand"
-#     value     = "npm start"
+#     namespace = "aws:elasticbeanstalk:environment"
+#     name      = "EnvironmentType"
+#     value     = "SingleInstance"  # Use 'LoadBalanced' for production
 #   }
 
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "EnvironmentType"
-    value     = "SingleInstance"  # Use 'LoadBalanced' for production
-  }
+#   setting {
+#     namespace = "aws:ec2:vpc"
+#     name      = "VPCId"
+#     value     = aws_vpc.main.id
+#   }
 
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = aws_vpc.main.id
-  }
+#   setting {
+#     namespace = "aws:ec2:vpc"
+#     name      = "Subnets"
+#     value     = aws_subnet.public-subnet-a.id
+#   }
 
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "Subnets"
-    value     = aws_subnet.public-subnet-a.id
-  }
+# setting {
+#     namespace = "aws:autoscaling:launchconfiguration"
+#     name      = "IamInstanceProfile"
+#     value     = aws_iam_instance_profile.eb_instance_profile.name
+#   }
 
-setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_instance_profile.eb_instance_profile.name
-  }
+#   setting {
+#     namespace = "aws:elasticbeanstalk:application:environment"
+#     name      = "S3_BUCKET"
+#     value     = aws_s3_bucket.memes.id
+#   }
 
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "S3_BUCKET"
-    value     = aws_s3_bucket.memes.id
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "S3_KEY"
-    value     = "api.zip"
-  }
-}
+#   setting {
+#     namespace = "aws:elasticbeanstalk:application:environment"
+#     name      = "S3_KEY"
+#     value     = "api.zip"
+#   }
+# }
 
--- terraform/aws/s3-bucket.tf
+++ terraform/aws/s3-bucket.tf
@@ -1,22 +1,32 @@
-resource "aws_s3_bucket" "memes" {
-    bucket = "${var.project}-memes-bucket"
-    force_destroy = true
-    versioning {
-        enabled = true
-    }
+# resource "aws_s3_bucket" "memes" {
+#     bucket = "${var.project}-memes-bucket"
+#     force_destroy = true
+#     versioning {
+#         enabled = true
+#     }
 
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
-    }
-}
+#     server_side_encryption_configuration {
+#         rule {
+#             apply_server_side_encryption_by_default {
+#                 sse_algorithm = "AES256"
+#             }
+#         }
+#     }
+# }
 
-resource "aws_s3_bucket_object" "my_app" {
-  bucket = aws_s3_bucket.memes.bucket
-  key    = "api.zip"
-  source = "./templates/api.zip"  # Path to your ZIP file on your local machine
-  etag   = filemd5("./templates/api.zip")
-}
+# resource "aws_s3_bucket" "memes" {
+#   bucket = "my-unique-bucket-name"  # Replace with your unique bucket name
+#   acl    = "public-read"            # This makes the bucket publicly readable
+
+# #   website {
+# #     index_document = "index.html"  # Specify the index document
+# #   }
+# }
+
+
+# resource "aws_s3_bucket_object" "my_app" {
+#   bucket = aws_s3_bucket.memes.bucket
+#   key    = "api.zip"
+#   source = "./templates/api.zip"  # Path to your ZIP file on your local machine
+#   etag   = filemd5("./templates/api.zip")
+# }
