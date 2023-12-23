894704362a46cc7dd731b29926fddb7d61262ad3
Tomas Ferrari <tferrari.92@gmail.com>
2023-12-23 03:19:34 +0100

    probando

--- terraform/aws/elastic-beanstalk.tf
+++ terraform/aws/elastic-beanstalk.tf
@@ -1,3 +1,32 @@
+resource "aws_iam_role" "eb_role" {
+  name = "my-eb-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eb_role_policy" {
+  role       = aws_iam_role.eb_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkWebTier"
+}
+
+resource "aws_iam_instance_profile" "eb_instance_profile" {
+  name = "my-eb-instance-profile"
+  role = aws_iam_role.eb_role.name
+}
+
+
+
 resource "aws_elastic_beanstalk_application" "myapp" {
   name        = "my-express-app"
   description = "My Express Application"
@@ -18,6 +47,12 @@ resource "aws_elastic_beanstalk_environment" "myenv" {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
     value     = "SingleInstance"  # Use 'LoadBalanced' for production
+  }
+
+setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.eb_instance_profile.name
   }
 
   setting {
