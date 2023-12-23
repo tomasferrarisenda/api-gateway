f6bbef724d347ba36577949a4867984f5b5181e8
Tomas Ferrari <tferrari.92@gmail.com>
2023-12-23 01:29:30 +0100

    probando

---
+++ terraform/aws/elastic-beanstalk.tf
@@ -0,0 +1,39 @@
+resource "aws_elastic_beanstalk_application" "myapp" {
+  name        = "my-express-app"
+  description = "My Express Application"
+}
+
+resource "aws_elastic_beanstalk_environment" "myenv" {
+  name                = "my-express-app-env"
+  application         = aws_elastic_beanstalk_application.myapp.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.3.6 running Node.js 14"  # Choose the relevant solution stack
+
+  setting {
+    namespace = "aws:elasticbeanstalk:container:nodejs"
+    name      = "NodeCommand"
+    value     = "npm start"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "SingleInstance"  # Use 'LoadBalanced' for production
+  }
+}
+
+
+resource "aws_elastic_beanstalk_environment" "myenv" {
+  # ... other configuration ...
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "S3_BUCKET"
+    value     = aws_s3_bucket.memes.name
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "S3_KEY"
+    value     = "path/to/your-app.zip"
+  }
+}
